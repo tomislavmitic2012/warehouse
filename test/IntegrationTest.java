bbddaa724a71b15e76a503da28d4f029f3c263c1
Tom Mitic <tomislavmitic2012@u.northwestern.edu>
2015-08-23 16:48:12 -0500

    Fix integration test

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -1,13 +1,7 @@
-import org.junit.*;
+import org.junit.Test;
 
-import play.mvc.*;
-import play.test.*;
-import play.libs.F.*;
-
+import static org.fest.assertions.Assertions.assertThat;
 import static play.test.Helpers.*;
-import static org.fest.assertions.Assertions.*;
-
-import static org.fluentlenium.core.filter.FilterConstructor.*;
 
 public class IntegrationTest {
 
@@ -17,11 +11,10 @@ public class IntegrationTest {
      */
     @Test
     public void test() {
-        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
-            public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333");
-                assertThat(browser.pageSource()).contains("Your new application is ready.");
-            }
+        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, browser -> {
+            browser.goTo("http://localhost:3333");
+            assertThat(browser.pageSource()).isNotNull();
+            assertThat(browser.pageSource()).isNotEmpty();
         });
     }
 
